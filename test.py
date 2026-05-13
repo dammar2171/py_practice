13ab87cdfcc5f52bf500fe5defa1104d738428ca
Dammar bhatt <dammarbhatt111@gmail.com>
2026-05-13 21:56:28 +0545

    Class and object concept started learning

---
+++ test.py
@@ -0,0 +1,17 @@
+# Exersise one
+# class Person:
+#   def __init__(self,name,age,city):
+#     self.name=name
+#     self.age=age
+#     self.city=city
+  
+#   def greet(self):
+#     print( f"Hi! I am {self.name}, I am {self.age} years old from {self.city}")
+
+# p1 = Person("Dammar",23,"MNR")
+# p2 = Person("Nirmala",25,"DDL")
+# p3 = Person("Dheeraj",22,"KTM")
+
+# p1.greet()
+# p2.greet()
+# p3.greet()
